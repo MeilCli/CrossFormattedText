e5607af23da96d88093c4c73aa33059e1df36e7c
meil <ics.sora.meil@gmail.com>
2016-12-23 18:41:58 +0900

    add sample and test code for WindowsPhone 8.1

---
+++ Test/Test.CrossFormattedText.WindowsPhone81/Test.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using Plugin.CrossFormattedText;
+using Windows.ApplicationModel.Core;
+using Windows.UI.Core;
+
+namespace Test.CrossFormattedText.WindowsPhone81 {
+
+    [TestClass]
+    public class Test {
+
+        [UITestMethod]
+        [TestMethod]
+        public void Pass() {
+            CrossCrossFormattedText.Current.Format(SpanText.HelloWorld);
+        }
+
+        [AttributeUsage(AttributeTargets.Method,AllowMultiple = false)]
+        public class UITestMethodAttribute : TestMethodAttribute {
+            public override TestResult[] Execute(ITestMethod testMethod) {
+                var task = ExecuteOnUi(testMethod);
+
+                task.Wait();
+
+                return task.Result;
+            }
+
+            private async Task<TestResult[]> ExecuteOnUi(ITestMethod testMethod) {
+                var tsc = new TaskCompletionSource<TestResult[]>();
+
+                await CoreApplication.MainView.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,() => {
+                    tsc.SetResult(base.Execute(testMethod));
+                });
+
+                return tsc.Task.Result;
+            }
+        }
+    }
+}
