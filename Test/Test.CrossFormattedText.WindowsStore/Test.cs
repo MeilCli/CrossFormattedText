322d1be8dd4f8847b95a724d4af3f12c56692f81
meil <ics.sora.meil@gmail.com>
2016-12-23 18:49:06 +0900

    add sample and test code for WindowsStore

---
+++ Test/Test.CrossFormattedText.WindowsStore/Test.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework.AppContainer;
+using Plugin.CrossFormattedText;
+
+namespace Test.CrossFormattedText.WindowsStore {
+
+    [TestClass]
+    public class Test {
+
+        [UITestMethod]
+        public void Pass() {
+            CrossCrossFormattedText.Current.Format(SpanText.HelloWorld);
+        }
+    }
+}
