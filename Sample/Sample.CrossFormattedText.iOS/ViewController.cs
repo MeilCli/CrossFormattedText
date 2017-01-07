0670f3113e08356b9de411a7ec41a79dfc854e9e
meil <ics.sora.meil@gmail.com>
2017-01-07 21:22:08 +0900

    add sample for iOS

---
+++ Sample/Sample.CrossFormattedText.iOS/ViewController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using UIKit;
+using Plugin.CrossFormattedText;
+
+namespace Sample.CrossFormattedText.iOS {
+    public partial class ViewController : UIViewController {
+        public ViewController(IntPtr handle) : base(handle) {
+        }
+
+        public override void ViewDidLoad() {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+            this.Label.AttributedText = CrossCrossFormattedText.Current.Format(SpanText.HelloWorld).Span();
+        }
+
+        public override void DidReceiveMemoryWarning() {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
