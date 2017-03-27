85e4c7e1f0d7a4bc72c3e984e06eed59eb3ff9a8
meil <ics.sora.meil@gmail.com>
2017-03-27 11:59:29 +0900

    split file

---
+++ Source/Plugin.CrossFormattedText.Abstractions/FontAttributes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plugin.CrossFormattedText.Abstractions {
+
+    [Flags]
+    public enum FontAttributes {
+        None = 1,
+        Bold = 1 << 1,
+        Italic = 1 << 2
+    }
+}
---
+++ Source/Plugin.CrossFormattedText.Abstractions/FontSize.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plugin.CrossFormattedText.Abstractions {
+
+    public struct FontSize {
+
+        public static readonly FontSize UltraBig = new FontSize(1.5f);
+        public static readonly FontSize Big = new FontSize(1.25f);
+        public static readonly FontSize Normal = new FontSize(1f);
+        public static readonly FontSize Small = new FontSize(0.75f);
+        public static readonly FontSize UltraSmall = new FontSize(0.5f);
+
+        public float Proportion { get; }
+
+        public FontSize(float proportion) {
+            Proportion = proportion;
+        }
+    }
+}
