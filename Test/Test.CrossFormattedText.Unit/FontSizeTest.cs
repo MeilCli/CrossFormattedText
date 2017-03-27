88ae122728773a5eae6836f8cc6b60f473ed5ebc
meil <ics.sora.meil@gmail.com>
2017-03-27 15:17:50 +0900

    add FontSize test

---
+++ Test/Test.CrossFormattedText.Unit/FontSizeTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Plugin.CrossFormattedText.Abstractions;
+
+namespace Test.CrossFormattedText.Unit {
+
+    [TestClass]
+    public class FontSizeTest {
+
+        [TestMethod]
+        public void EqualsTest() {
+            FontSize a1 = new FontSize(1.1f);
+            FontSize a2 = new FontSize(1.1f);
+            FontSize b = new FontSize(1.11f);
+            Assert.AreEqual(a1,a2);
+            Assert.AreNotEqual(a1,b);
+        }
+
+        [TestMethod]
+        public void OperateTest() {
+            FontSize a1 = new FontSize(1.1f);
+            FontSize a2 = new FontSize(1.1f);
+            FontSize b = new FontSize(1.11f);
+            Assert.AreEqual(a1 == a2,true);
+            Assert.AreEqual(a1 != b,true);
+        }
+    }
+}
