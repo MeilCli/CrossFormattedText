4f4d73dec39b1d9218a34f22fa6464f644fe8db4
meil <ics.sora.meil@gmail.com>
2017-03-27 15:05:14 +0900

    extend Span class

--- Source/Plugin.CrossFormattedText.Abstractions/Span.cs
+++ Source/Plugin.CrossFormattedText.Abstractions/Span.cs
@@ -7,7 +7,7 @@ using System.Windows.Input;
 
 namespace Plugin.CrossFormattedText.Abstractions {
 
-    public class Span {
+    public class Span : IEquatable<Span> {
 
         public string Text { get; set; } = string.Empty;
 
@@ -25,6 +25,58 @@ namespace Plugin.CrossFormattedText.Abstractions {
         /// If use Command, require this
         /// </summary>
         public object CommandParameter { get; set; }
+
+        public override bool Equals(object obj) {
+            if(obj == null) {
+                return false;
+            }
+            if(obj is Span) {
+                return Equals((Span)obj);
+            }
+            return false;
+        }
+
+        public bool Equals(Span span) {
+            if(span == null) {
+                return false;
+            }
+            if(ReferenceEquals(this,span)) {
+                return true;
+            }
+            if(Text != span.Text) {
+                return false;
+            }
+            if(BackgroundColor != span.BackgroundColor) {
+                return false;
+            }
+            if(ForegroundColor != span.ForegroundColor) {
+                return false;
+            }
+            if(FontAttributes != span.FontAttributes) {
+                return false;
+            }
+            if(FontSize.Proportion != span.FontSize.Proportion) {
+                return false;
+            }
+            if(Command != span.Command) {
+                return false;
+            }
+            if(CommandParameter != span.CommandParameter) {
+                return false;
+            }
+            return true;
+        }
+
+        public override int GetHashCode() {
+            int hash = Text?.GetHashCode() ?? -1;
+            hash ^= BackgroundColor.GetHashCode();
+            hash ^= ForegroundColor.GetHashCode();
+            hash ^= FontAttributes.GetHashCode();
+            hash ^= FontSize.Proportion.GetHashCode();
+            hash ^= Command?.GetHashCode() ?? -1;
+            hash ^= CommandParameter?.GetHashCode() ?? -1;
+            return hash;
+        }
 
     }
 
