18b2bcdbbfafe21f6d9089a13bb9a61504a1e9e8
meil <ics.sora.meil@gmail.com>
2017-05-02 18:54:03 +0900

    add FormattedStringBuilder and Append method

--- Source/Plugin.CrossFormattedText.Abstractions/FormattedStringBuilder.cs
+++ Source/Plugin.CrossFormattedText.Abstractions/FormattedStringBuilder.cs
@@ -8,18 +8,167 @@ namespace Plugin.CrossFormattedText.Abstractions {
 
     public class FormattedStringBuilder {
 
-        private List<Span> _spans;
+        private CharSpan[] values;
 
-        public List<Span> Spans {
-            get {
-                if(_spans == null) {
-                    throw new ArgumentNullException(nameof(Spans));
+        public int Capacity => values.Length;
+        public int Length { get; private set; }
+
+        public FormattedStringBuilder() : this(0) { }
+
+        public FormattedStringBuilder(int capacity) {
+            values = new CharSpan[capacity];
+        }
+
+        private void expandCapacity(int minimumCapacity) {
+            int newCapacity = (Capacity + 1) * 2;
+            if(minimumCapacity > newCapacity) {
+                newCapacity = minimumCapacity;
+            }
+
+            var newArray = new CharSpan[newCapacity];
+            Array.Copy(values,newArray,Length);
+            values = newArray;
+        }
+
+        public void EnsureCapacity(int minimumCapacity) {
+            if(minimumCapacity > Capacity) {
+                expandCapacity(minimumCapacity);
+            }
+        }
+
+        internal FormattedString MergeCharSpan() {
+            var list = new List<Span>();
+            Span currentSpan = null;
+            var sb = new StringBuilder();
+            for(int i = 0;i < Length;i++) {
+                CharSpan c = values[i];
+                if(currentSpan != null && ReferenceEquals(currentSpan,c.Span) == false) {
+                    Span span = currentSpan.Clone();
+                    span.Text = sb.ToString();
+                    sb.Clear();
+                    currentSpan = null;
+                    list.Add(span);
                 }
-                return _spans;
+                if(currentSpan == null) {
+                    currentSpan = c.Span;
+                }
+                sb.Append(c.Character);
             }
-            set {
-                _spans = value;
+            if(currentSpan != null) {
+                Span span = currentSpan.Clone();
+                span.Text = sb.ToString();
+                list.Add(span);
             }
+            return new FormattedString(list.ToArray());
+        }
+
+        public void Append(bool value) {
+            Append(value,null);
+        }
+
+        public void Append(bool value,Span span) {
+            Append(value.ToString(),span);
+        }
+
+        public void Append(byte value) {
+            Append(value,null);
+        }
+
+        public void Append(byte value,Span span) {
+            Append(value.ToString(),span);
+        }
+
+        public void Append(char value) {
+            Append(value,null);
+        }
+
+        public void Append(char value,Span span) {
+            EnsureCapacity(Length + 1);
+
+            span = span ?? new Span();
+            values[Length] = new CharSpan(value,span);
+            Length += 1;
+        }
+
+        public void Append(decimal value) {
+            Append(value,null);
+        }
+
+        public void Append(decimal value,Span span) {
+            Append(value.ToString(),span);
+        }
+
+        public void Append(double value) {
+            Append(value,null);
+        }
+
+        public void Append(double value,Span span) {
+            Append(value.ToString(),span);
+        }
+
+        public void Append(float value) {
+            Append(value,null);
+        }
+
+        public void Append(float value,Span span) {
+            Append(value.ToString(),span);
+        }
+
+        public void Append(short value) {
+            Append(value,null);
+        }
+
+        public void Append(short value,Span span) {
+            Append(value.ToString(),span);
+        }
+
+        public void Append(int value) {
+            Append(value,null);
+        }
+
+        public void Append(int value,Span span) {
+            Append(value.ToString(),span);
+        }
+
+        public void Append(long value) {
+            Append(value,null);
+        }
+
+        public void Append(long value,Span span) {
+            Append(value.ToString(),span);
+        }
+
+        public void Append(object value) {
+            Append(value,null);
+        }
+
+        public void Append(object value,Span span) {
+            if(value == null) {
+                throw new ArgumentNullException(nameof(value));
+            }
+            Append(value.ToString(),span);
+        }
+
+        public void Append(string value) {
+            Append(value,null);
+        }
+
+        public void Append(string value,Span span) {
+            if(value == null) {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            EnsureCapacity(Length + value.Length);
+
+            span = span ?? new Span();
+            CharSpan[] ar = value.ToCharArray().Select(x => new CharSpan(x,span)).ToArray();
+
+            Array.Copy(ar,0,values,Length,ar.Length);
+            Length += ar.Length;
+        }
+
+        public FormattedString ToFormattedString() {
+            return MergeCharSpan();
         }
     }
 }
---
+++ Test/Test.CrossFormattedText.Unit/FormattedStringBuilderTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Plugin.CrossFormattedText.Abstractions;
+
+namespace Test.CrossFormattedText.Unit {
+
+    [TestClass]
+    public class FormattedStringBuilderTest {
+
+        [TestMethod]
+        public void AppendTest() {
+            var sb = new StringBuilder();
+            var fsb = new FormattedStringBuilder();
+
+            sb.Append(true);
+            fsb.Append(true);
+            sb.Append((byte)10);
+            fsb.Append((byte)10);
+            sb.Append((decimal)10.0);
+            fsb.Append((decimal)10.0);
+            sb.Append(3.14);
+            fsb.Append(3.14);
+            sb.Append(3.14f);
+            fsb.Append(3.14f);
+            sb.Append((short)10);
+            fsb.Append((short)10);
+            sb.Append(19);
+            fsb.Append(19);
+            sb.Append(1L);
+            fsb.Append(1L);
+            sb.Append((object)"aaa");
+            fsb.Append((object)"aaa");
+            sb.Append('1');
+            fsb.Append('1');
+            sb.Append("text text");
+            fsb.Append("text text");
+
+            Assert.AreEqual(fsb.ToFormattedString().Text,sb.ToString());
+        }
+    }
+}
